6492d3458eac13a0421449f4be22622a42c71acb
GosegoMatlokwe <gosegomatlokwe@gmail.com>
2026-08-07 04:34:16 -0400

    Implemented Search Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,2 +1,5 @@
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
+
 <Route path="/app/product/:productId" element={<DetailsPage/>} />
+<Route path="/app/search" element={<SearchPage/>} />
